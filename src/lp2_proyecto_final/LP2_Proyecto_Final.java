65e5996eb4aeaf9cdc338ebb074d6c08b2b4b505
René Pauls <paulsmrene@live.com>
2020-10-03 14:39:01 -0400

    Commit inicial con codigo prueba

---
+++ src/lp2_proyecto_final/LP2_Proyecto_Final.java
@@ -0,0 +1,43 @@
+
+package lp2_proyecto_final;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/*
+ * @author Manuel René Pauls Toews
+ */
+public class LP2_Proyecto_Final {
+
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            File f = new File("chinook.db");
+            String url = "jdbc:sqlite:"+f.getAbsolutePath();
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+
+}
